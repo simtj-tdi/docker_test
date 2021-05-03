822f08306fd6432d83469b15dbde97f7bf968d74
simtj-tdi <60678325+simtj-tdi@users.noreply.github.com>
2021-05-04 04:33:32 +0900

    commit

--- www/html/index.php
+++ www/html/index.php
@@ -1,3 +1,20 @@
-<?
+<?php 
+$host = 'gabiatest_db_1'; // service name from docker-compose.yml
+$user = 'exampleuser'; 
+$password = 'examplepass'; 
+$db = 'exampledb'; 
+$conn = new mysqli($host, $user, $password, $db); 
+if($conn -> connect_error){ 
+    echo 'connection failed' . $conn -> connect_error;
+} else {
+    echo 'Sucessfully connected to MYSQL';
+}
+
+// $link = mysql_connect('gabiatest_db_1', 'root', 'examplepass');
+// if (!$link) {
+//     die('Could not connect: ' . mysql_error());
+// }
+// echo 'Connected successfully';
+
 phpinfo();
 ?>
